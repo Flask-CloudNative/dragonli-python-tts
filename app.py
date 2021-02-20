6648116d0d2b28e8dfe53475a8ede226126ae762
hekaiyou <hekaiyou@qq.com>
2021-02-20 19:39:43 +0800

    支持选择语言

--- app.py
+++ app.py
@@ -19,42 +19,43 @@ def api_tts():
     parameter = Parameter(request)
 
     if parameter.method == 'GET':
-        if parameter.verification(checking=parameter.param_url, verify={'text': str}):
-            param = parameter.param_url
-            # 根据文档是否存在
-            if db.does_it_exist(docu={'text': param['text']}):
-                documen = db.mongo_collection.find_one({'text': param['text']})
-                # 返回.wav音频
-                return Response(documen['wav'], mimetype="audio/x-wav")
-            else:
-                tts = ToSpeech()
-                # TTS操作
-                stream = tts.recognize(text=param['text'])
-                # 文件保存操作
-                wav_path = os.path.join(
-                    app.config['SEPPCH_DIR'], str(uuid.uuid4()) + '.wav'
-                )
-                # 判断TTS是否成功
-                if not stream.can_read_data(requested_bytes=1):
-                    return '文本转语音失败', 500, []
-                # 保存文件到服务指定目录
-                stream.save_to_wav_file(wav_path)
-                # 文件写入Mongo数据库
-                with open(wav_path, 'rb') as f_wav:
-                    content = io.BytesIO(f_wav.read())
-                    insert_dict = {
-                        'text': param['text'],
-                        'wav': bson.binary.Binary(content.getvalue())
-                    }
-                # 删除文件
-                os.remove(wav_path)
-                result = db.write_one_docu(docu=insert_dict)
-                if result:
-                    return Response(insert_dict['wav'], mimetype="audio/x-wav")
-                else:
-                    return '语音合成文件无法被保存', 500, []
+        param = parameter.verification(checking=parameter.param_url, verify={
+                                       'text': str, 'language': str}, optional={'language': 'auto'})
+        # 根据文档是否存在
+        if db.does_it_exist(docu={'text': param['text'], 'language': param['language']}):
+            documen = db.mongo_collection.find_one(
+                {'text': param['text'], 'language': param['language']})
+            # 返回.wav音频
+            return Response(documen['wav'], mimetype="audio/x-wav")
         else:
-            return '请求参数错误', 400, []
+            tts = ToSpeech()
+            # TTS操作
+            stream = tts.recognize(
+                text=param['text'], language=param['language'])
+            # 文件保存操作
+            wav_path = os.path.join(
+                app.config['SEPPCH_DIR'], str(uuid.uuid4()) + '.wav'
+            )
+            # 判断TTS是否成功
+            if not stream.can_read_data(requested_bytes=1):
+                return '文本转语音失败', 500, []
+            # 保存文件到服务指定目录
+            stream.save_to_wav_file(wav_path)
+            # 文件写入Mongo数据库
+            with open(wav_path, 'rb') as f_wav:
+                content = io.BytesIO(f_wav.read())
+                insert_dict = {
+                    'text': param['text'],
+                    'language': param['language'],
+                    'wav': bson.binary.Binary(content.getvalue())
+                }
+            # 删除文件
+            os.remove(wav_path)
+            result = db.write_one_docu(docu=insert_dict)
+            if result:
+                return Response(insert_dict['wav'], mimetype="audio/x-wav")
+            else:
+                return '语音合成文件无法被保存', 500, []
 
 
 if __name__ == '__main__':
--- utils/to_speech.py
+++ utils/to_speech.py
@@ -13,12 +13,20 @@ class ToSpeech():
             region=self.service_region,
         )
 
-    def recognize(self, text):
-        synthesizer = SpeechSynthesizer(
-            speech_config=self.speech_config,
-            audio_config=None,
-            auto_detect_source_language_config=AutoDetectSourceLanguageConfig()
-        )
+    def recognize(self, text, language: str = 'auto'):
+        if language != 'auto':
+            self.speech_config.speech_synthesis_language = language[:5]
+            self.speech_config.speech_synthesis_voice_name = language
+            synthesizer = SpeechSynthesizer(
+                speech_config=self.speech_config,
+                audio_config=None,
+            )
+        else:
+            synthesizer = SpeechSynthesizer(
+                speech_config=self.speech_config,
+                audio_config=None,
+                auto_detect_source_language_config=AutoDetectSourceLanguageConfig()
+            )
         result = synthesizer.speak_text_async(text).get()
         stream = AudioDataStream(result)
         return stream
